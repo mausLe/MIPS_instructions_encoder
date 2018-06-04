29c6cd24b866f02013423271390fb735a4b6d090
Mariusz Bielecki <maniekb12@gmail.com>
2018-06-04 23:48:16 +0200

    Projekt 1 skonczony

--- 288728_mariusz_bielecki/README.md
+++ 288728_mariusz_bielecki/README.md
@@ -1,3 +1,13 @@
-Aby odpalić w folderze z testem >python3 assembler.py plik_z_testami
+## Wykonane zadania: 
+1
+## Wymagania:
+Python 3 (program testowany był na python 3.6.3)
 
-niestety nie mam testów. wstawiłbym mema opisujacego mój projekt, ale mam 5 minut.
+## Uruchamianie:  
+    python3 assembler.py tests/tests.in (lub inny plik)  
+## testowanie z moimi testami za pomocą makefile:  
+    make test (zakładam, że w PATH istnieje zmienna python3)
+## wyczyszczenie katalogu po przetestowaniu:
+    make clean
+
+P.S. obrazek, który zamieściłem w komentarzu na SKOS miał raczej sens wczoraj, dziś ten program już nie wygląda tak okropnie.
--- 288728_mariusz_bielecki/assembler.py
+++ 288728_mariusz_bielecki/assembler.py
@@ -1,21 +1,9 @@
-from string import Template
-from constants import INSTRUCTIONS, REGISTERS
+from constants import INSTRUCTIONS, REGISTERS, BLOCKS_FORMATS
 import re
 import sys
 
 
 class Instruction:
-
-    blocks_formats = {
-        'R': ['{0:06b}', '{0:05b}',
-              '{0:05b}', '{0:05b}',
-              '{0:05b}', '{0:06b}'],
-
-        'I': ['{0:06b}', '{0:05b}',
-              '{0:05b}', '{0:016b}'],
-
-        'J': ['{0:06b}', '{0:026b}']
-    }
 
     def __init__(self, metadata):
         data = metadata.split(':')
@@ -25,76 +13,90 @@ class Instruction:
 
     def generate_bin_template(self, meta_template):
         blocks = meta_template.split('/')
-        lengths = Instruction.blocks_formats[self.instr_type]
+        lengths = BLOCKS_FORMATS[self.instr_type]
         return ''.join(('{' + block[1:] + form[2:]
                         if block[0] == '%'
                         else block)
                        for form, block in zip(lengths, blocks))
 
-    def encode_hex(self, arguments):
+    def encode_to_int(self, arguments):
         u2_repr = map(lambda x: x if x >= 0 else x % (1 << 16), arguments)
-        #print(self.template, u2_repr)
-        return hex(int((self.template).format(*u2_repr), 2))
+        # print(self.template, u2_repr)
+        return int((self.template).format(*u2_repr), 2)
 
 
-def get_prg_from_file(file):
+class Assembler:
 
-    def is_instruction_canditate(instr):
+    def __init__(self, prog_path, instr_set):
+        self.instruction_dict = self.__generate_instructions_dict(instr_set)
+        self.program = self.__get_prg_from_file(prog_path)
+
+    def __generate_instructions_dict(self, instr_set):
+        instr_dict = {}
+        for instruction_meta in INSTRUCTIONS:
+            splitted_instr = instruction_meta.split(":")
+            instr_dict[splitted_instr[0]] = Instruction(instruction_meta)
+        return instr_dict
+
+    def __get_prg_from_file(self, file):
+
+        with open(file, 'r') as f:
+            lines = list(map(str.strip, map(str.casefold, f.readlines())))
+            lines_with_instr = [line
+                                for line in lines
+                                if self.__is_instruction_canditate(line)]
+            prog = list(enumerate(lines_with_instr))
+        return prog
+
+    def __is_instruction_canditate(self, instr):
         stripped = instr.strip()
         if stripped == '' or stripped[0] == '#':
             return False
         else:
             return True
 
-    with open(file, 'r') as f:
-        lines = list(map(str.strip, map(str.casefold, f.readlines())))
-        lines_with_instr = [line
-                            for line in lines
-                            if is_instruction_canditate(line)]
-        prog = list(enumerate(lines_with_instr))
-    return prog
+    def assemble(self):
+        print('.text')
+        for line, instr in self.program:
+            splitted_instr = re.split('\s+|\s*,\s*', instr)
+            try:
+                instruction_instance = self.instruction_dict[splitted_instr[0]]
+            except KeyError:
+                raise Exception("unrecognised instruction: " +
+                                str(instr))
 
+            def get_int_arg(arg):
+                if arg[0] == '$':
+                    try:
+                        return REGISTERS[arg]
+                    except KeyError:
+                        raise Exception(
+                            "Invalid register in instr:" + str(instr))
+                else:
+                    try:
+                        return (int(arg, 16)
+                                if arg[:2] == "0x" or arg[1:3] == "0x"
+                                else int(arg))
+                    except ValueError:
+                        raise Exception(
+                            "Invalid immediate value in instr: " + str(instr))
 
-def assembler(prog, instructions_dict):
-    print('.text')
-    for line, instr in prog:
-        splitted_instr = re.split('\s+|\s*,\s*', instr)
-        try:
-            instruction_instance = instr_dict[splitted_instr[0]]
-        except KeyError:
-            raise Exception("unrecognised instruction no." + str(line))
-        num_of_args = len(instruction_instance.operands)
-        args = splitted_instr[1:num_of_args + 1]
-        #print(args)
-
-        def encode_arg(arg):
-            if arg[0] == '$':
-                try:
-                    return REGISTERS[arg]
-                except KeyError:
-                    raise Exception("Invalid register in instr no." + str(line))
-            else:
-                try:
-                    return int(arg)
-                except ValueError:
-                    raise Exception("Invalid immediate value in instr no." + str(line))
-        encoded_args = map(encode_arg, args)
-        encoded_instr = '{0:08X}'.format(
-            int(instruction_instance.encode_hex(encoded_args), 16))
-        encoded_instr_num = '{0:08X}'.format(4*line)
-        print("  "+
-            encoded_instr_num+"  ",
-            encoded_instr+"  ",
-            instr
-        )
+            num_of_args = len(instruction_instance.operands)
+            args = splitted_instr[1:num_of_args + 1]
+            if num_of_args != len(args):
+                raise Exception("too few arguments in instr: " + str(instr))
+            encoded_args = map(get_int_arg, args)
+            encoded_instr = '{0:08X}'.format(
+                instruction_instance.encode_to_int(encoded_args))
+            encoded_instr_num = '{0:08X}'.format(4*line)
+            print("  " +
+                  encoded_instr_num+"  ",
+                  encoded_instr+"  ",
+                  instr
+                  )
 
 
 if __name__ == '__main__':
-
-    instr_dict = {}
-    for instruction_meta in INSTRUCTIONS:
-        splitted_instr = instruction_meta.split(":")
-        instr_dict[splitted_instr[0]] = Instruction(instruction_meta)
-
-    prog = get_prg_from_file(sys.argv[1])
-    assembler(prog, instr_dict)
+    prog_path = sys.argv[1]
+    asm_instance = Assembler(prog_path, INSTRUCTIONS)
+    asm_instance.assemble()
